67a054e20ff62cf23a83b1f228ab90e205dbee2f
ghostonbeat <syabanaminggus7@gmail.com>
2021-10-30 16:23:19 +0700

    Bahan Ajar

---
+++ prak4/AJAX/contoh.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Extraordinary You";
+?>
---
+++ prak4/AJAX/experiment.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#btn_tampil').click(function(){
+        $('#tampil').load('contoh.php');
+    });
+});
