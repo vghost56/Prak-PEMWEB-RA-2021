3776a7b913171b0521a2aa5be50c72e3c0484a24
ghostonbeat <syabanaminggus7@gmail.com>
2021-10-30 17:41:38 +0700

    Bahan Ajar

--- prak4/AJAX/experiment.js
+++ prak4/AJAX/experiment.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
         alert('Data : '+data+'\nStatus : '+status);
     });
 });
+
+$(document).ready(function(){
+    $('#btn_tampil').click(function(){
+        $.post('info.php', {nama : "Putri", alamat : "Gg. Mangga 2"},
+        function(data, status) {
+            alert('Data : '+data+'\nStatus : '+status);
+        });
+    });
+});
