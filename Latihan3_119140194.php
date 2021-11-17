49847a273f57a0f8c14d160638d9f4ada6c13cf7
vghost56 <64187072+vghost56@users.noreply.github.com>
2021-11-17 23:29:58 +0700

    Add files via upload

---
+++ Latihan3_119140194.php
@@ -0,0 +1,30 @@
+<?php 
+    function prima($data){
+        for($n=1; $n <= $data; $n++){
+            $temp = true;
+
+            if($n == 2){
+                echo " $n ";
+            }
+            
+            else if($n != 1){
+                $limit = ($n-1);
+                for($j=2; $j < $limit; $j++){
+                    if($n%$j == 0){
+                        $temp = false;
+                        break;
+                    }
+                }
+
+                if($temp == true){
+                    echo " , $n ";
+                }
+            }
+
+        }
+    }
+
+    echo "<p> Bilangan prima dari 1 - 50 adalah : ";
+    echo "<br></br>";
+    prima(50);
+?>
